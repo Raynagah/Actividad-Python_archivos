fbc8d4bcb90b72863d857a18ea107f52c7a2748d
Carlos <ca.moil@duocuc.cl>
2024-06-17 12:04:02 -0400

    funcion Agregar

--- funciones_de_main.py
+++ funciones_de_main.py
@@ -3,3 +3,12 @@
 def listar_trabajadores():
     print("hola");
 #wena ctm
+
+def agregarTrabajadores():
+    #Pedir datos
+    nombre=input("Agregue el nombre: ")
+    apellido=input("Agregue el apellido: ")
+    cargo=input("Agregue el cargo del trabajador: ")
+    sueldob=input("Agregue el sueldo bruto del trabajador: ")
+    #VALIDAR ENTRADA DE DATOS
+    
