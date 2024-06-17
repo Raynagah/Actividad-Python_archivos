79c8a69d23bb83c921f1a63b2477afe661e07b2f
Mayckol <may.mardones@duocuc.cl>
2024-06-17 12:03:37 -0400

    login_main

--- login.py
+++ login.py
@@ -8,17 +8,25 @@ lista_user = ["luis1010", "4321"]
 lista_user2 = ["marcelo123", "2021"]
 lista_user3 = ["juan2020", "6789"]
 
+print("\nLogin")
+
 while flag:
 
-    print("\nLogin")
     user = input("Ingrese su usuario: ")
     password = input("Ingrese su contraseña: ")
 
     if user == lista_user[0] and password == lista_user[1]:
-        print(f"El {user} ")
+        print(f"\nEl usuario {user} ha ingresado correctamente.\n")
+        flag = False
+
     elif user == lista_user2[0] and password == lista_user2[1]:
-        print(f"El {user} ")
+        print(f"\nEl usuario {user} ha ingresado correctamente.\n")
+        flag = False
+
     elif user == lista_user3[0] and password == lista_user3[1]:
-        print(f"El {user} ")
+        print(f"\nEl usuario {user} ha ingresado correctamente.\n")
+        flag = False
+
     else:
-        print("Vuelva a intentarlo...")
+         print("Vuelva a intentarlo...\n")
+
--- main_actividad.py
+++ main_actividad.py
@@ -13,6 +13,38 @@ lista_cargos=["CEO","Jefe de área","Obrero","Contador"];
 
 bandera_menu=True;
 
+"""
+Login
+"""
+
+flag = True
+
+lista_user = ["luis1010", "4321"]
+lista_user2 = ["marcelo123", "2021"]
+lista_user3 = ["juan2020", "6789"]
+
+print("\n~~~Login~~~")
+
+while flag:
+
+    user = input("Ingrese su usuario: ")
+    password = input("Ingrese su contraseña: ")
+
+    if user == lista_user[0] and password == lista_user[1]:
+        print(f"\nEl usuario {user} ha ingresado correctamente.\n")
+        flag = False
+
+    elif user == lista_user2[0] and password == lista_user2[1]:
+        print(f"\nEl usuario {user} ha ingresado correctamente.\n")
+        flag = False
+
+    elif user == lista_user3[0] and password == lista_user3[1]:
+        print(f"\nEl usuario {user} ha ingresado correctamente.\n")
+        flag = False
+
+    else:
+         print("Vuelva a intentarlo...\n")
+
 while bandera_menu:
     print("\n*****Elige una de las siguientes opciones*****\n");
     print("1.Registrar Trabajador");
