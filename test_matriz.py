c93d10ebcc552aeb604b78d06ec62ca427a7523a
mayckol <may.mardones@duocuc.cl>
2024-06-17 21:45:58 -0400

    validacion pe

--- test_matriz.py
+++ test_matriz.py
@@ -1,12 +1,37 @@
 import json
 
+flag = True
+flag1 = True
+flag2 = True
+flag3 = True
 
-nombre = input("Agregue el nombre: ")
-apellido = input("Agregue el apellido: ")
-cargo = input("Agregue el cargo del trabajador: ")
-sueldob = input("Agregue el sueldo bruto del trabajador: ")
+while flag1:
+    nombre = input("Agregue el nombre: ")
+    if nombre == "":
+        print("Este campo es obligatorio...")
+    else:
+        flag1 = False
 
+while flag2:
+    apellido = input("Agregue el apellido: ")
+    if apellido == "":
+        print("Este campo es obligatorio...")
+    else:
+        flag2 = False
 
+while flag3:
+    cargo = input("Agregue el cargo del trabajador: ")
+    if cargo == "":
+        print("Este campo es obligatorio...")
+    else:
+        flag3 = False
+
+while flag:
+    try:
+        sueldob = int(input("Agregue el sueldo bruto del trabajador: "))
+        flag = False 
+    except:
+        print("El sueldo ingresado debe ser un número entero...")
 
 # Crear el nuevo trabajador
 nuevo_trabajador = {"nombre": nombre, "apellido": apellido, "cargo": cargo, "sueldob": sueldob}
